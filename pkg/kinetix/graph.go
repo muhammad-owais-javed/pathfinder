4e0dbd7dea30f31fb70824f673f7699628821f16
Owais <contact@owais.fi>
2026-07-09 23:33:08 +0300

    Update Graph Data Structure

--- pkg/kinetix/graph.go
+++ pkg/kinetix/graph.go
@@ -22,17 +22,50 @@ type Graph struct {
 
 func NewGraph() *Graph{
 	return &Graph{
-		Nodes: make(map[string]bool)
+		Nodes: make(map[string]*Node)
 		coordsTracker: make(map[string]bool)
 	}
 }
 
 func (g *Graph) AddNode(name string, x, y int) error {
 
+	if _, exists := g.Nodes[name]; exists == true {
+		return fmt.Errorf("Duplicate station name found: %s", name)
+	}
+
+	coordKey := fmt.Sprintf("%d,%d", x, y)
+
+	if g.coordsTracker[coordsKey] {
+		return fmt.Error("Duplicate coordinates found at %s for station %s", coordKey, name)
+	}
+
+	newNode := *Node {
+		Name : name,
+		X : x,
+		Y : y,
+		Edges : make([]*Node, 0),
+	}
+
+	g.Nodes[name] = newNode
+	g.coordsTracker[coordsKey] = true
+
 	return nil
 }
 
 func (g *Graph) AddEdge(name1, name2 string) error {
 
+	node1, exists1 := g.Nodes[name1]
+	node2, exists2 := g.Nodes[name2]
+
+	if !exists1{
+		return fmt.Sprintf("Cannot create edge, station doesn't exists: %s", name1)
+	}
+
+	if !exists2{
+		return fmt.Sprintf("Cannot create edge, station doesn't exists: %s", name2)
+	}
+
+	node1.Edges := append(node1.Edges, node2)
+	node2.Edges := append(node2.Edges, node1)
 	return nil
 }
