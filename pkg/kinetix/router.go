896ab2c708e5a9741da63ddc7a14e02250b94635
Owais <contact@owais.fi>
2026-07-13 21:49:49 +0300

    Add BFS logic

--- pkg/kinetix/router.go
+++ pkg/kinetix/router.go
@@ -9,8 +9,8 @@ func (g *Graph) FindShortestPath(startName string, endName string) ([]*Node, err
 	startNode, startExists := g.Nodes[startName]
 	endNode, endExists := g.Nodes[endName]
 
-	if !startsExists {
-		return nil, Errorf("Start station does not exist: %s", startName)
+	if !startExists {
+		return nil, fmt.Errorf("Start station does not exist: %s", startName)
 	}
 	
 	if !endExists {
@@ -21,5 +21,49 @@ func (g *Graph) FindShortestPath(startName string, endName string) ([]*Node, err
 		return nil, fmt.Errorf("Start and End stations are the same: %s", startName)
 	}
 
+	queue := []*Node{startNode}
+
+	visited := make(map[string]bool)
+	visited[startName] = true
+
+	parent := make(map[string]*Node)
+
+	found := false
+
+	for len(queue) > 0 {
+
+		currentNode := queue[0]
+		queue = queue[1:]
+
+		if currentNode.Name == endName {
+			found = true
+			break
+		}
+
+		for _, neighbor := range currentNode.Edges {
+
+			if visited[neighbor.Name] == false {
+				visited[neighbor.Name] = true
+				parent[neighbor.Name] = currentNode
+				queue = append(queue, neighbor)
+			}
+
+		}
+
+		if found == false {
+			continue
+		}
+
+	}
+
+	var path []*Node
+	currentNode := endNode
+
+	for currentNode != nil {
+		path = append(path, currentNode)
+		currentNode = parent[currentNode.Name]
+	}
+
+	return path, nil
 
 }
